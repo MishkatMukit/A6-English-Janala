cdf4153d45f5cfa39e469a79789ec16f122edad1
MishkatMukit <mahabubmishkat8@gmail.com>
2025-10-08 03:54:29 +0600

    added modal and voice

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,10 @@
  const loadAllLevels = async()=>{
+    showLessonLoader()
     const response = await fetch('https://openapi.programming-hero.com/api/levels/all')
     const alldata = await response.json()
+    hideLessonLoader()
     displayAllLevels(alldata.data)
+    
 }
 const displayAllLevels = (levels) =>{
     const lessonContainer = document.getElementById('lesson-container')
@@ -9,7 +12,7 @@ const displayAllLevels = (levels) =>{
         const btnlevel = document.createElement('button')
         
         btnlevel.innerHTML=`
-            <button id="btn-${level.id}" onclick="showLessons(${level.level_no},${level.id})"
+            <button id="btn-${level.id}" onclick="LoadLessons(${level.level_no},${level.id})"
                     class="btn hover:bg-[#422AD5] hover:text-white btn-sm flex items-center gap-1 text-sm text-[#422AD5] border-2 font-semibold rounded-md py-1 px-4 border-[#422AD5]"><i
                         class="fa-solid fa-book-open"></i> Lesson -${level.level_no
 }</button>
@@ -17,11 +20,52 @@ const displayAllLevels = (levels) =>{
         lessonContainer.appendChild(btnlevel)
     });
 }
+const showWordDetails= async (id)=>{
 
-const showLessons = async (lessonID, btnID) =>{
+    const response = await fetch(`https://openapi.programming-hero.com/api/word/${id}`)
+
+    const alldata = await response.json()
+
+    console.log(alldata.data)
+    const details = alldata.data
+    const modal = document.getElementById('word_details')
+     console.log(modal)
+     modal.innerHTML=`
+        <div class="modal-box">
+                        <h3 class="font-semibold text-2xl">${details.word} (<i class="fa-solid fa-microphone"></i> :${details.pronunciation})</h3>
+                        <div class="py-3">
+                            <p class="font-medium text-xl">Meaning</p>
+                            <p class="font-siliguri font-medium text-lg">${details.meaning}</p>
+                        </div>
+                        <div class="pb-3">
+                            <p class="font-medium text-xl">Example</p>
+                            <p class=" font-normal text-md">${details.sentence}</p>
+                        </div>
+                        <div class="pb-3 mb-3 space-y-2">
+                            <p class="font-medium font-siliguri text-xl">সমার্থক শব্দ গুলো</p>
+                            <p id="syn-container" class=" font-normal text-sm space-x-1">
+                                
+                                ${details.synonyms.map(word => `
+          <span class="py-1 px-2 bg-[#EDF7FF] border-1 border-[#D7E4EF] rounded-md">${word}</span>
+        `).join('')}
+                            </p>
+                        </div>
+                        <div class="modal-action-left">
+                            <form method="dialog" class="flex">
+                                <!-- if there is a button in form, it will close the modal -->
+                                <button class="btn bg-[#422AD5] text-white font-medium">Complete Learning</button>
+                            </form>
+                        </div>
+                    </div>
+     `
+
+
+    word_details.showModal()
+}
+const LoadLessons = async (lessonID, btnID) =>{
     removeActiveClass()
     const activeButton = document.getElementById(`btn-${btnID}`)
-    console.log(activeButton)
+    // console.log(activeButton)
     activeButton.classList.add('active')
     showWordsLoader()
     const response = await fetch(`https://openapi.programming-hero.com/api/level/${lessonID}`)
@@ -48,9 +92,9 @@ const displayLessons = (words)=>{
                         <p class="font-medium text-xl">Meaning /Pronounciation</p>
                         <p class="font-semibold text-2xl font-siliguri">"${word.meaning} / ${word.pronunciation}"</p>
                         <div class="flex justify-between">
-                            <button class="btn p-3 bg-[#1A91FF20] rounded-lg text-lg"><i
+                            <button  onclick="showWordDetails(${word.id})" class="btn p-3 bg-[#1A91FF20] rounded-lg text-lg"><i
                                     class="fa-solid fa-circle-info"></i></button>
-                            <button class="btn border-none p-3 bg-[#1A91FF20] rounded-lg text-lg"><i
+                            <button onclick="pronounceWord('${word.word}')" class="btn border-none p-3 bg-[#1A91FF20] rounded-lg text-lg"><i
                                     class="fa-solid fa-volume-high"></i></button>
                         </div>
                     </div>
--- scripts/utilities.js
+++ scripts/utilities.js
@@ -17,3 +17,19 @@ const hideWordsLoader=()=>{
     document.getElementById('words-container').classList.remove("hidden")
 
 }
+const showLessonLoader=()=>{
+    document.getElementById('lesson-loader').classList.remove("hidden")
+    document.getElementById('lesson-container').classList.add("hidden")
+
+}
+const hideLessonLoader=()=>{
+    document.getElementById('lesson-loader').classList.add("hidden")
+    document.getElementById('lesson-container').classList.remove("hidden")
+
+}
+const pronounceWord = (word) => {
+    console.log(word)
+      const utterance = new SpeechSynthesisUtterance(word);
+      utterance.lang = 'en-EN'; // English
+      window.speechSynthesis.speak(utterance);
+    }
